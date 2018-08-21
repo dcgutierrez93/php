f2a45c211bd23bd45200b81255df831dda066c30
dcgutierrez93 <dcgutierrez93@yahoo.com>
2018-08-21 15:13:46 -0500

    Added Variables Section

---
+++ variables/index.php
@@ -0,0 +1,14 @@
+<?php
+
+// Name Information
+$first = 'David';
+$last = 'Gutierrez';
+$full = "$first $last";
+
+// Age Information
+$birth_year = 1993;
+$current_year = 2018;
+$age = $current_year - $birth_year;
+
+echo "My name is $full.\n";
+echo "I am $age years old.\n";
